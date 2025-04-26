3751a0e94d6e643b9ff714d7e4c95991072aa174
Caponmada <services-client@caponmada.com>
2025-04-26 09:52:06 +0300

    fatal3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
+  ssr : false,
   modules: ["@pinia/nuxt", "nuxt-aos", "nuxt-swiper"],
   compatibilityDate: "2025-04-09"
 })
