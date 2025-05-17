ff325356c5917e7bcfdeaef23687fafaf943d6fc
Nomenjanahary S <jsdev.web2@gmail.com>
2025-05-17 21:56:48 +0300

    rect2

--- components/some-image.vue
+++ components/some-image.vue
@@ -1,71 +1,25 @@
 <template>
     <section class="row overflow-hidden">
-        <div class="col-11 d-flex m-auto flex-nowrap">
-            <div id="first" data-aos="fade-up-right" class="col-lg-3 col-md-5 col-11 border rounded m-1 items-1 overflow-hidden">
-                <div class="bg-primary col-12">
-                    <div class="items d-flex">
-                        <div class="col-12 d-flex align-items-end">
-                            <div class="col-12 d-flex justify-content-end">
-                                <i class="bi bi-arrow-down-right-circle-fill fs-2 m-2"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="contain d-flex col-12 bg-success">
-                        <div class="col-12 d-flex flex-wrap align-items-end p-2">
-                            <h3 class="text-light col-12 text-center"> Safety First </h3>
-                            <p class="text-light fw-bold text-center">
-                                We prioritize the safety of our travelers at every step of their adventure. Our tours are carefully planned to minimize risks and provide a worry-free experience.
-                            </p>
-                            <div class="col-12 d-flex justify-content-end">
-                                <i class="bi bi-arrow-up-right-circle-fill fs-1"></i>
-                            </div>
-                        </div>
-                    </div>
+        <div class="col-12 d-flex gap-1">
+            <div class="col-lg-4 col-6 rounded border m-auto">
+                <img loading="lazy" class="col-12 m-0 p-0" src="./some-image-inset/vecteezy_lemur.jpg" alt="">
+                <div class="col-12 bg-success text-center text-light">
+                        <h3 class="text-center">{{ val[0].title }}</h3>
+                        <span class="col-12 fw-bold"> {{ val[0].content }} </span>
                 </div>
             </div>
-            <div id="second" data-aos="fade-up" class="col-lg-3 col-md-5 d-lg-block d-block  col-11 border rounded m-1 items-2 overflow-hidden">
-                <div class="bg-primary col-12">
-                    <div class="items d-flex">
-                        <div class="col-12 d-flex align-items-end">
-                            <div class="col-12 d-flex justify-content-end">
-                                <i class="bi bi-arrow-down-right-circle-fill fs-2 m-2"></i>
-                                   </div>
-                        </div>
-                    </div>
-                    <div class="contain d-flex col-12 bg-success">
-                        <div class="col-12 d-flex flex-wrap align-items-end p-2">
-                            <h3 class="text-light col-12 text-center"> Environmental Respect </h3>
-                            <p class="text-light fw-bold text-center">
-                                By choosing our agency, you are opting for responsible tourism. We are committed to protecting the environment and promoting sustainable practices that benefit both nature and local communities.
-                            </p>
-                            <div class="col-12 d-flex justify-content-end">
-                                <i class="bi bi-arrow-up-right-circle-fill fs-1"></i>
-                            </div>
-                        </div>
-                    </div>
+            <div class="col-lg-4 col-6 rounded border m-auto">
+                <img loading="lazy" class="col-12 m-0 p-0" src="./some-image-inset/vecteezy_black-lemur.jpg" alt="">
+                <div class="col-12 bg-success text-center text-light">
+                        <h3 class="text-center">{{ val[1].title }}</h3>
+                        <span class="col-12 fw-bold"> {{ val[1].content }} </span>
                 </div>
             </div>
-            <div id="third" data-aos="fade-up-left" class="col-lg-3 d-lg-block d-block col-11 border rounded m-1 items-3 overflow-hidden">
-                <div class="bg-primary col-12">
-                    <div class="items d-flex">
-                        <div class="col-12 d-lg-none d-md-none d-flex justify-content-end"><i class="bi bi-x-square"></i></div>
-                        <div class="col-12 d-flex align-items-end">
-                            <div class="col-12 d-flex justify-content-end">
-                                <i class="bi bi-arrow-down-right-circle-fill fs-2 m-2"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="contain d-flex col-12 bg-success">
-                        <div class="col-12 d-flex flex-wrap align-items-end p-2">
-                            <h3 class="text-light col-12 text-center"> Local Expertise </h3>
-                            <p class="text-light fw-bold text-center">
-                                With years of on-the-ground experience, our team knows Madagascar inside and out. Our local guides are passionate experts who will help you discover hidden treasures and share the island's secrets with you.
-                            </p>
-                            <div class="col-12 d-flex justify-content-end">
-                                <i class="bi bi-arrow-up-right-circle-fill fs-1"></i>
-                            </div>
-                        </div>
-                    </div>
+                <div class="col-lg-4 col-md-6 col-11 d-none d-lg-block rounded border m-auto">
+                <img loading="lazy" class="col-12 m-0 p-0" src="./some-image-inset/couche_soleil_baobab.jpg" alt="">
+                <div class="col-12 bg-success text-center text-light">
+                        <h3 class="text-center">{{ val[2].title }}</h3>
+                        <span class="col-12 fw-bold"> {{ val[2].content }} </span>
                 </div>
             </div>
         </div>
@@ -83,63 +37,24 @@ import type { some_component_type } from '~/all_model/model';
 
 </script>
 <style scoped>
-section {
-    padding: 5px 20px;
-    scrollbar-color: red gray;
-    scrollbar-width: thin;
-    overflow: hidden;
-    display: block;
+    section.row .col-12 .col-lg-4 {
+        overflow: hidden;
+        outline: none;
+        height: 45vh;
+        color : black
+    }
+    section.row .col-12 .col-lg-4 .col-12 {
+        height: 100%;
+        transition: 0.4s;
+    }
+        /* section.row .col-12 .col-lg-4 .col-12:nth-child(2) {
+            padding: 40px 5px;
 
-}
-.row .col-11 {
-    height: 20rem;
-    flex-wrap: wrap;
-    gap: 5px;
-    justify-content: center;
-}
-.row .col-11 .col-lg-3 {
-    background-position: center;
-    background-size: cover;
-}
-.row .col-11 .col-lg-3 .bg-primary {
-    display: block;
-    height: 200%;
-    transition: 0.5s;
-}
-.row .col-11 .col-lg-3:hover > .bg-primary {
-    transform: translateY(-50%);
-    transition: 0.5s;
-}
-.row .col-11 .items-1 .bg-primary .items {
-    background-image: url(./some-image-inset/vecteezy_lemur.jpg);
-    background-position: center;
-    background-size: cover;
-    display: block;
-    height: 50%;
-}
-.row .col-11 .items-2 .bg-primary .items {
-    background-image: url(./some-image-inset/vecteezy_black-lemur.jpg);
-    background-position: center;
-    background-size: cover;
-    display: block;
-    height: 50%;
-}
-.row .col-11 .items-3 .bg-primary .items {
-    background-image: url(./some-image-inset/couche_soleil_baobab.jpg);
-    background-position: center;
-    background-size: cover;
-    display: block;
-    height: 50%;
-}
-.row .col-11 .col-lg-3 .bg-primary .contain {
-    display: block;
-    height: 50%;
-}
-.row .col-11 .col-lg-3 .bg-primary .contain:active {
-    transform: translateY(50%);
-    transition: 0.5s;
-}
-.row .col-11 .col-lg-3 .bg-primary .contain:hover > .col-12 {
-    color: #1492E6;
-}
+        } */
+    section.row .col-12 .col-lg-4:hover > .col-12 {
+        transform: translateY(-100%);
+        transition: 0.5s;
+    }
+
+
 </style>
