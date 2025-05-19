e1686f28d6b03a41ffc9d49cbb76a396ee2e85e6
Nomenjanahary S <jsdev.web2@gmail.com>
2025-05-19 22:02:57 +0300

    farany-ngamba

--- components/some-image.vue
+++ components/some-image.vue
@@ -1,22 +1,58 @@
 <template>
  <div ref="container" class="keen-slider">
     <div class="keen-slider__slide number-slide1 border">
-        <img  class="col-12" src="./some-image-inset/vecteezy_cameleon_copy.jpg" alt="">
+      <div class="col-12 d-block m-0 p-0">
+            <img  class="col-12" src="./some-image-inset/IMG_20230923_094118_767.jpg" alt=""> 
+            <div class="col-12 bg-success m-0 p-0">
+              <h3 class="fw-bold text-center">{{ array[0].title  }}</h3>
+              <div class="col-12 text-center">{{ array[0].content }}</div>
+            </div>
+      </div>
     </div>
-    <div class="keen-slider__slide number-slide2">
-        <img  class="col-12" src="./some-image-inset/vecteezy_lemur.jpg" alt="">
+        <div class="keen-slider__slide number-slide1 border">
+      <div class="col-12 d-block m-0 p-0">
+        <img  class="col-12" src="./some-image-inset/vecteezy_ring-tailed-.jpg" alt="">
+            <div class="col-12 bg-success m-0 p-0">
+              <h3 class="fw-bold text-center">{{ array[1].title  }}</h3>
+              <div class="col-12 text-center">{{ array[1].content }}</div>
+            </div>
+      </div>
     </div>
-    <div class="keen-slider__slide number-slide3">
+        <div class="keen-slider__slide number-slide1 border">
+      <div class="col-12 d-block m-0 p-0">
         <img  class="col-12" src="./some-image-inset/vecteezy_black-lemur.jpg" alt="">
+            <div class="col-12 bg-success m-0 p-0">
+              <h3 class="fw-bold text-center">{{ array[2].title  }}</h3>
+              <div class="col-12 text-center">{{ array[2].content }}</div>
+            </div>
+      </div>
     </div>
-    <div class="keen-slider__slide number-slide4">
-        <img  class="col-12" src="./some-image-inset/couche_soleil_baobab.jpg" alt="">
+        <div class="keen-slider__slide number-slide1 border">
+      <div class="col-12 d-block m-0 p-0">
+        <img  class="col-12" src="./some-image-inset/IMG_20230923_142926_953.jpg" alt="">
+            <div class="col-12 bg-success m-0 p-0">
+              <h3 class="fw-bold text-center">{{ array[3].title  }}</h3>
+              <div class="col-12 text-center">{{ array[3].content }}</div>
+            </div>
+      </div>
     </div>
-    <div class="keen-slider__slide number-slide5">
+        <div class="keen-slider__slide number-slide1 border">
+      <div class="col-12 d-block m-0 p-0">
         <img  class="col-12" src="./some-image-inset/baobab_lena copy.jpg" alt="">
+            <div class="col-12 bg-success m-0 p-0">
+              <h3 class="fw-bold text-center">{{ array[4].title  }}</h3>
+              <div class="col-12 text-center">{{ array[4].content }}</div>
+            </div>
+      </div>
     </div>
-    <div class="keen-slider__slide number-slide6">
+        <div class="keen-slider__slide number-slide1 border">
+      <div class="col-12 d-block m-0 p-0">
         <img  class="col-12" src="./some-image-inset/capsM.jpeg" alt="">
+            <div class="col-12 bg-success m-0 p-0">
+              <h3 class="fw-bold text-center">{{ array[6].title  }}</h3>
+             <div class="col-12 text-center">{{ array[6].content }}</div>
+            </div>
+      </div>
     </div>
   </div>
 </template>
@@ -33,13 +69,22 @@ export default {
         perView: 2,
       },
     })
-    return { container }
+    const array = [
+                {title : "Safety First:" ,content : "We prioritize the safety of our travelers at every step of their adventure. Our tours are carefully planned to minimize risks and provide a worry-free experience.",image : "/some-image-inset/vecteezy_cameleon_copy.jpg"},
+                {title : "Local Expertise: " ,content : " With years of on-the-ground experience, our team knows Madagascar inside and out. Our local guides are passionate experts who will help you discover hidden treasures and share the island's secrets with you."},
+                {title : "Personalized Journeys: " ,content : " Every traveler is unique, and we believe every adventure should be too. We offer tailor-made tours that are adapted to your desires and pace, ensuring a truly unforgettable experience."},
+                {title : "Environmental Respect: " ,content : "By choosing our agency, you are opting for responsible tourism. We are committed to protecting the environment and promoting sustainable practices that benefit both nature and local communities."},
+                {title : "Safety First:" ,content : "We prioritize the safety of our travelers at every step of their adventure. Our tours are carefully planned to minimize risks and provide a worry-free experience.",image : "/some-image-inset/vecteezy_cameleon_copy.jpg"},
+                {title : "Local Expertise: " ,content : " With years of on-the-ground experience, our team knows Madagascar inside and out. Our local guides are passionate experts who will help you discover hidden treasures and share the island's secrets with you."},
+                {title : "Personalized Journeys: " ,content : " Every traveler is unique, and we believe every adventure should be too. We offer tailor-made tours that are adapted to your desires and pace, ensuring a truly unforgettable experience."}
+    ]
+    return { container,array }
   }
 }
 </script>
 
 <style scoped>
-body {
+/* body {
   margin: 0;
   font-family: 'Inter', sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -56,6 +101,20 @@ body {
   font-weight: 500;
   height: 300px;
   max-height: 100vh;
+} */
+ .keen-slider .number-slide1 {
+  height: 300px;
+ }
+.keen-slider .number-slide1 .col-12 {
+    height: 200%;
+    font-weight: bold;
+    transition: 0.5s;
+    }
+.keen-slider .number-slide1:hover > .col-12 {
+  transform: translateY(-50%);
+  transition: 0.5s;
 }
-img {height: 100%;}
+.keen-slider .number-slide1 .col-12 .col-12 {
+  height: 50%;
+}
 </style>
